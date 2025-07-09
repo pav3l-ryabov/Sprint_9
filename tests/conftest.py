8216c6243253bc345c5f63d6ce79b1ecfc6ccc5b
Pavel Ryabov <pav3l.ryabov@yandex.ru>
2025-07-09 22:21:46 +0300

    fix 3

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,3 @@
-# import pytest
-# from selenium import webdriver
-#
-#
-# @pytest.fixture
-# def driver():
-#     driver_instance = webdriver.Chrome()
-#     yield driver_instance
-#     driver_instance.quit()
-
 import os
 import pytest
 from selenium import webdriver
